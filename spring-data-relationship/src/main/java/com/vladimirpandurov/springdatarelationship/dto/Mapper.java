17dd95cda592caf8346e40490ae2cb308fd70f43
panvla <pandvlado@gmail.com>
2023-02-15 15:47:02 +0100

    added mapper for dto.

--- spring-data-relationship/src/main/java/com/vladimirpandurov/springdatarelationship/dto/Mapper.java
+++ spring-data-relationship/src/main/java/com/vladimirpandurov/springdatarelationship/dto/Mapper.java
@@ -1,8 +1,11 @@
 package com.vladimirpandurov.springdatarelationship.dto;
 
+import com.vladimirpandurov.springdatarelationship.dto.responseDto.AuthorResponseDto;
 import com.vladimirpandurov.springdatarelationship.dto.responseDto.BookResponseDto;
+import com.vladimirpandurov.springdatarelationship.dto.responseDto.CategoryResponseDto;
 import com.vladimirpandurov.springdatarelationship.model.Author;
 import com.vladimirpandurov.springdatarelationship.model.Book;
+import com.vladimirpandurov.springdatarelationship.model.Category;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +25,55 @@ public class Mapper {
         return bookResponseDto;
     }
 
+    public static List<BookResponseDto> booksToBookResponseDtos(List<Book> books){
+        List<BookResponseDto> bookResponseDtos = new ArrayList<>();
+        for(Book book: books){
+            bookResponseDtos.add(bookToBookResponseDto(book));
+        }
+        return bookResponseDtos;
+    }
 
+    public static AuthorResponseDto authorToAuthorResponseDto(Author author){
+        AuthorResponseDto authorResponseDto = new AuthorResponseDto();
+        authorResponseDto.setId(author.getId());
+        authorResponseDto.setName(author.getName());
+        List<String> names = new ArrayList<>();
+        List<Book> books = author.getBooks();
+        for(Book book: books){
+            names.add(book.getName());
+        }
+        authorResponseDto.setBookNames(names);
+        return authorResponseDto;
+    }
+
+    public static List<AuthorResponseDto> authorsToAuthorResponseDtos(List<Author> authors){
+        List<AuthorResponseDto> authorResponseDtos = new ArrayList<>();
+        for(Author author: authors){
+            authorResponseDtos.add(authorToAuthorResponseDto(author));
+        }
+        return authorResponseDtos;
+    }
+
+    public static CategoryResponseDto categoryToCategoryResponseDto(Category category){
+        CategoryResponseDto categoryResponseDto = new CategoryResponseDto();
+        categoryResponseDto.setId(category.getId());
+        categoryResponseDto.setName(category.getName());
+        List<String> names = new ArrayList<>();
+        List<Book> books = category.getBooks();
+        for(Book book : books){
+            names.add(book.getName());
+        }
+        categoryResponseDto.setBookNames(names);
+        return categoryResponseDto;
+    }
+
+    public static List<CategoryResponseDto> categoryToCategoryResponseDtos(List<Category> categories){
+        List<CategoryResponseDto> categoryResponseDtos = new ArrayList<>();
+        for(Category category: categories){
+            categoryResponseDtos.add(categoryToCategoryResponseDto(category));
+        }
+        return categoryResponseDtos;
+    }
 
 
 }
