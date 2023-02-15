a347111ad4964465b54195985e2e6d29851568cb
panvla <pandvlado@gmail.com>
2023-02-15 13:45:58 +0100

    added dto classes.

---
+++ spring-data-relationship/src/main/java/com/vladimirpandurov/springdatarelationship/dto/requestDto/ZipcodeRequestDto.java
@@ -0,0 +1,9 @@
+package com.vladimirpandurov.springdatarelationship.dto.requestDto;
+
+import lombok.Data;
+
+@Data
+public class ZipcodeRequestDto {
+    private String name;
+    private Long cityId;
+}
